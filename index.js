567ce4d90bd6f82e2bd29237376869f5e011d835
Erik Tonnesen <estonnesen@gmail.com>
2022-12-15 14:40:53 -0500

    added some js code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const button = document.getElementById('button')
+
+let val = 10;
+
+button.addEventListener('click', function () {
+    let input = document.getElementById('num');
+    if(input === val) {
+        console.log('this is the correct number');
+    }
+});
 
